c1b5a247974be3c52081dfeab8db7e2338785d9b
Mahmoud-Reeda <redamahmoud6789@gmail.com>
2025-12-11 16:09:07 +0200

    workflow duplication

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more || Docker React App || and tags
+        Click on the Vite and React logos to learn more || Docker React App || and tags || check duplicaition workflow
       </p>
     </>
   )
